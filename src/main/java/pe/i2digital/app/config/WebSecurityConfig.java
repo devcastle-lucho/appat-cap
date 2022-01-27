092fcd58bf610ffb34d3967a1af65a0445028e20
dev_dastle <luis_castle_2011@hotmai.com>
2022-01-26 22:02:57 -0500

    Spring Security ignorar la seguridad
    [Descripcion] Ignorar la autenticacion por defecto de spring security.
    [Sesion] Spring boot
    [Tema] Seguridad - Spring Security

--- src/main/java/pe/i2digital/app/config/WebSecurityConfig.java
+++ src/main/java/pe/i2digital/app/config/WebSecurityConfig.java
@@ -5,16 +5,21 @@
  */
 package pe.i2digital.app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     
     //Autenticacion S1: Basico Auth // En Memoria
-    @Override
+    /*@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .passwordEncoder(passwordEncoder())
@@ -22,9 +27,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
                 .password(passwordEncoder().encode("--#--"))
                 .roles("ADMIN", "USER");
                 ;
-    }
+    }*/
     
+    @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    /*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .httpBasic();
+    }*/ 
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+    
 }
